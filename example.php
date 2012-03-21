38bf5c3d219ba7489fc25ac04ae8232c9fc30f4d
Roger Stringer <roger@freekrai.net>
2012-03-21 09:17:53 -0700

    updated example

--- example.php
+++ example.php
@@ -8,13 +8,8 @@ include("Lawnchair.php");
 $ppl = new Lawnchair( array("name"=>"people","store"=>"file") );
 
 if( $ppl->count() < 1 ){
-	$ppl->save( array("value"=>array("name"=>"Roger") ) );
-	$ppl->save( array("value"=>array("name"=>"Kaitlyn") ) );
-	$ppl->save( array("value"=>array("name"=>"Patsy") ) );
-	$ppl->save( array("value"=>array("name"=>"Bailey") ) );
-	
 	for($i = 0; $i <= 15000;$i++){
-		$ppl->save( array("value"=>array("name"=>$i) ) );
+		$ppl->save( array("value"=>array("name"=>$i,"age"=>($i+2),"address"=>"random street") ) );
 	}
 	
 }
@@ -29,8 +24,8 @@ echo "<h1>Find all people with 'a' in the name </h1>";
 $list = $ppl->find(array("field"=>"name","q"=>"a","a"=>"eq"));
 echo "<pre>".print_r($list,true)."</pre>";
 
-#echo "<h1>List All People</h1>";
-#echo "<pre>".print_r($ppl->all(),true)."</pre>";
+echo "<h1>List All People</h1>";
+echo "<pre>".print_r($ppl->all(),true)."</pre>";
 
 echo "Last Record is: ".print_r($ppl->max(),true)."<br />";
 echo "Last Key is: ".$ppl->lastid();
